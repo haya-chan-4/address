a0ed8c0346aabf0b37447701a4afc56d8bb3a889
Hayata <hayata8812@gmail.com>
2020-11-21 13:01:01 +0900

    header 改造

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,46 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-side-icon></v-toolbar-side-icon>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>マイアドレス帳</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-toolbar>
+  <div>
 
-    <v-content>
-    </v-content>
+    <v-app>
+      <v-app-bar
+        app
+        color="primary"
+        dark
+      >
+
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-title>
+        <h1>Address Book</h1>
+      </v-app-bar-title>
+
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
+
+    </v-app-bar>
+
+    <v-main>
+
+    </v-main>
   </v-app>
+  </div>
 </template>
 
 <script>
+
+
 export default {
   name: 'App',
+
   components: {
+
   },
-  data () {
-    return {
-      //
-    }
-  }
-}
+
+  data: () => ({
+    //
+  }),
+};
 </script>
